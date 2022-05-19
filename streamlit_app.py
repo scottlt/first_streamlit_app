c43235a2b59b92556bfd190f0af2f2339504d235
scottlt <49419061+scottlt@users.noreply.github.com>
2022-05-18 20:25:34 -0500

    Update streamlit_app.py
    added load list button

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,6 @@ def get_fruityvice_data(this_fruit_choice):
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
         return fruityvice_normalized
    
-
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
@@ -44,14 +43,22 @@ except URLError as e:
     streamlit.error()
 
 streamlit.write('The user entered', fruit_choice)
-streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#Snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx_cursor() as my_cur:
+        my_cur.execute("SELECT * from fruit_load_list")
+        return my_cur.fetchall()
+
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+
+streamlit.stop()
 
 add_fruit = streamlit.text_input('What fruit would you like add?', 'jackfruit')
 streamlit.write("Thanks for adding", add_fruit)
