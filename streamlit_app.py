1dcbf4fa97b3051e093df29b4998bf9a5550ac25
scottlt <49419061+scottlt@users.noreply.github.com>
2022-05-18 20:28:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ import requests
 from urllib.error import URLError
 
 def get_fruityvice_data(this_fruit_choice):
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        return fruityvice_normalized
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
    
 streamlit.title('My Parents New Healthy Diner')
 
